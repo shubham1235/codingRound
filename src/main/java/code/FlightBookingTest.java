97d892185c1b0dffdaaf75b739d377c3fc17e9a4
shubham verma <shubhamverma815@gmail.com>
2018-05-15 10:38:10 +0530

    Remove some Import Lib Add GenericFuction.waiFor from GenericFucntion class Remove Waifor Methode beacuse this method created in Genericfunction class remoce set driverPath beacuse PlatfromUtil.mac and PlatfromUtil.isWindows,Platfrom.IsLinux is depreciated Signed-off-by: shubham verma <shubhamverma815@gmail.com>

--- src/main/java/code/FlightBookingTest.java
+++ src/main/java/code/FlightBookingTest.java
@@ -1,40 +1,43 @@
 package code;
-import com.sun.javafx.PlatformUtil;
-import com.sun.jna.Platform;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class FlightBookingTest {
 
     WebDriver driver;
+    
+    GenericFucntion generic = new GenericFucntion();
+    
+    
+    
+    
+    @BeforeMethod
+    public void setAllMethod()
+    {
+    	this.driver = GenericFucntion.setDriverPath();
+    	
+    }
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
 
-        setDriverPath();
+  
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.get("https://www.cleartrip.com/");
 
-        waitFor(2000);
+       GenericFucntion.waitFor(2000);
         
         driver.findElement(By.id("OneWay")).click();
 
@@ -45,7 +48,7 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the origin
 
-        waitFor(2000);
+        GenericFucntion.waitFor(2000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
@@ -54,7 +57,7 @@ public class FlightBookingTest {
 
         //wait for the auto complete options to appear for the destination
 
-        waitFor(2000);
+        GenericFucntion.waitFor(2000);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
@@ -64,22 +67,13 @@ public class FlightBookingTest {
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
+        GenericFucntion.waitFor(5000);
         //verify that result appears for the provided journey search
         Assert.assertTrue(isElementPresent(By.className("searchSummary")));
 
         //close the browser
         driver.quit();
 
-    }
-
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
     }
 
 
@@ -92,28 +86,6 @@ public class FlightBookingTest {
         }
     }
 
-    private void setDriverPath() {
-    	
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");          
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-        
-      // this is for (disable notification popup )
-       
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("profile.default_content_setting_values.notifications", 1);
-        ChromeOptions options = new ChromeOptions();
-        options.setExperimentalOption("prefs", prefs);
-       
-        driver = new ChromeDriver(options);	
-        
-    }
     
   ///  
     public static void WaitFor_visibility(WebDriver driver, By elementLocator) {
