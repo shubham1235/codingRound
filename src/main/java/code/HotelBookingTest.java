68cc2ace13348c94dbb1dda9ed1edaa7b94cc2ad
shubham verma <shubhamverma815@gmail.com>
2018-05-17 12:45:53 +0530

    remove unused Import lib also clean code Signed-off-by: shubham verma <shubhamverma815@gmail.com>

--- src/main/java/code/HotelBookingTest.java
+++ src/main/java/code/HotelBookingTest.java
@@ -1,54 +1,77 @@
 package code;
-import com.sun.javafx.PlatformUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
-    WebDriver driver = new ChromeDriver();
+	WebDriver driver;
+// this class contain generic function 
+	// also contain selenium driver so we no need to create again and again for every test class
+	GenericFucntion generic = new GenericFucntion();
 
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+	@Test
+	public void shouldBeAbleToSearchForHotels() {
 
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
+		//set driver from GenericFunction class
+		this.driver = GenericFucntion.setDriverPath();
 
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
+		// create object for HotelBooking element also gave driver to that class for element 
+		HotelBookingElement bookingElement = PageFactory.initElements(driver, HotelBookingElement.class);
 
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
+		// this method wait 160 second for website loading if website take too much time for opening so it mean soting wrong with network
+		driver.manage().timeouts().pageLoadTimeout(160, TimeUnit.SECONDS);
 
-    @Test
-    public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+		driver.get("https://www.cleartrip.com/");
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+		//click on hotel booking link
+		GenericFucntion.WaitFor_visibilityOfElements(driver, bookingElement.getHotelLink());
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
+		
+		bookingElement.getHotelLink().click();
 
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+		// wail till element will be visible 
+		GenericFucntion.WaitFor_visibilityOfElements(driver, bookingElement.getLocalityTextBox());
 
-        driver.quit();
+		//search hotel or location based hotel
+		bookingElement.getLocalityTextBox().sendKeys("Indiranagar, Bangalore");
 
-    }
+		GenericFucntion.waitFor(3000);
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
+		// get all search result in drop box the we will select particular element
+		bookingElement.getSearchResult().click();
+
+		GenericFucntion.waitFor(2000);
+
+		// get today date by system this is basic java code for date
+		String timeStamp = new SimpleDateFormat("dd").format(Calendar.getInstance().getTime());
+
+		System.out.println("hello this is " + timeStamp);
+
+		// select date which we want to use for hotel booking(from and  to date)
+		bookingElement.clickselectdate(timeStamp, "30");
+
+		GenericFucntion.waitFor(1000);
+
+		// select how many person want hotel
+		new Select(bookingElement.getTravellerSelection()).selectByVisibleText("1 room, 2 adults");
+
+		GenericFucntion.waitFor(1000);
+
+		//search hotel
+		bookingElement.getSearchButton().click();
+
+		// wait 5 second for search hotel result 
+		GenericFucntion.waitFor(5000);
+
+		driver.quit();
+
+	}
 
 }
